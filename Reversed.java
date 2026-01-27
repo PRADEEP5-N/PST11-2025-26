5a8bdd67772b6f0ea2ba8d1df92ee18caad100de
PRADEEP5-N <pradeepnarayanan2006@gmail.com>
2026-01-27 09:32:39 +0530

    Add files via upload

---
+++ Reversed.java
@@ -0,0 +1,12 @@
+package PST;
+
+public class Reversed {
+       public static void main(String[] args) {
+	   String var= "hello";
+		for(int i=var.length()-1;i>0;i--) {
+			System.out.println(var.charAt(i));
+		}
+
+	}
+
+}
