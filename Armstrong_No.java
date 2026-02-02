4c1321394cc99c509b0840e94f8b97522cb81df5
PRADEEP5-N <pradeepnarayanan2006@gmail.com>
2026-02-02 09:31:08 +0530

    Add files via upload

---
+++ Armstrong_No.java
@@ -0,0 +1,28 @@
+package PSTproject;
+
+import java.util.Scanner;
+
+public class Armstrong_No {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int num = sc.nextInt();
+		 int originalNum = num;
+	        int result = 0;
+	        int n = String.valueOf(num).length(); 
+	        
+	        while (num > 0) {
+	            int digit = num % 10;
+	            result += Math.pow(digit, n);
+	            num /= 10;
+	        }	        
+	        if (result == originalNum) {
+	            System.out.println(originalNum + " is an Armstrong number.");
+	        } else {
+	            System.out.println(originalNum + " is NOT an Armstrong number.");
+	        }
+	        
+	        sc.close();
+	    }
+	}	              
